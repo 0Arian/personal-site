4f8538ecc19b0717590121cd448d3cc45d5a18ab
arianmoslem <amoslem678@gmail.com>
2017-09-06 15:59:20 -0400

    Fix: Updated Projects + Links formatting

--- src/pages/links.js
+++ src/pages/links.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "gatsby-link";
+import './link.css';
 
 const Links = () => (
   <div
@@ -14,42 +15,55 @@ const Links = () => (
     />
     <h2>Links I found useful</h2>
     <h3>Youtube Links</h3>
-    <p>
-      -{" "}
-      <a
-        href="https://www.youtube.com/channel/UC3QLHt6mHfmg4x_h2am7ecg"
-        className="bodyLink"
-      >
-        Sesh Venugopal
-      </a>, Professor at Rutgers University with a lot of useful vieos on data
-      structures.<br />
-      -{" "}
-      <a href="https://www.youtube.com/user/patrickJMT" className="bodyLink">
-        PatrickJMT
-      </a>, Former Math Instructor who makes videos explaining topics. Useful
-      for catching up in classes and learning many other cool topics
-    </p>
-    <h3>Web Links</h3>
-    <p>
-      -{" "}
+    <ul>
+      <li>
+        <a
+          href="https://www.youtube.com/channel/UC3QLHt6mHfmg4x_h2am7ecg"
+          className="bodyLink"
+        >
+          Sesh Venugopal
+        </a>, Professor at Rutgers University with a lot of useful vieos on data
+        structures.
+      </li>
+      <li>
+        <a href="https://www.youtube.com/user/patrickJMT" className="bodyLink">
+          PatrickJMT
+        </a>, Former Math Instructor who makes videos explaining math topics.
+        Useful for catching up in classes and learning many other cool topics
+      </li>
+    </ul>
+    <div>
+      <h3>Web Links</h3>
+    </div>
+    <ul><li>
       <a
         href="https://github.com/angrave/SystemProgramming/wiki"
         className="bodyLink"
-      >
-        Systems Programming Notes
+      >Systems Programming Notes
       </a>, Useful for learning Systems Programming and catching up in class for
-      every missed lecture. Also a useful resource for learning C<br />
-      -{" "}
+      every missed lecture. Also a useful resource for learning C.
+      </li><li>
       <a href="http://www.diveintopython3.net/" className="bodyLink">
         Dive Into Python3 Book
       </a>, Available for free on the web, this book is how I learned python 3.
       It goes over refactoring, unit testing, regular expressions, and many
-      other useful built in python tools.<br />
-      -{" "}
+      other useful built in python tools.
+      </li><li>
       <a href="https://techdevguide.withgoogle.com/" className="bodyLink">
         Google's Tech Development Guide
-      </a>, Very useful for practicing common interview questions and finding resources to grow your tech skills<br />
-    </p>
+      </a>, Very useful for practicing common interview questions and finding
+      resources to grow your tech skills
+      </li><li>
+      <a href="https://github.com/zeeshanu/learn-regex" className="bodyLink">
+        Learn Regex the Easy Way
+      </a>, a great way to learn regular expressions
+      </li><li>
+      <a href="https://github.com/getify/You-Dont-Know-JS" className="bodyLink">
+        You-Dont-Know-JS
+      </a>, Several books on learning Javascript. Includes Async/Performance and
+      ES6 material.
+      </li>
+    </ul>
   </div>
 );
 
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -34,18 +34,31 @@ const projects = () => (
     </div>
     <div>
       <h2>
-        <a className="bodyLink" href="https://github.com/ARM-open/Graphite">Graphite</a>
+        <a className="bodyLink" href="https://github.com/ARM-open/Graphite">
+          Graphite
+        </a>
       </h2>
       <p>
         A tool for scraping google analytics data and generating a meaningful
         report for your usage data.
       </p>
-      <img src="https://camo.githubusercontent.com/52e26fc8b1a0add5d051b76f5ec827bc6a956f88/687474703a2f2f692e696d6775722e636f6d2f794972446d45632e706e67"
+      <img
+        src="https://camo.githubusercontent.com/52e26fc8b1a0add5d051b76f5ec827bc6a956f88/687474703a2f2f692e696d6775722e636f6d2f794972446d45632e706e67"
         alt=""
         height="622"
         width="526"
-      >
-      </img>
+      />
+    </div>
+    <div>
+      <h2>
+        <a className="bodyLink" href="https://github.com/AirrBear/PicGenie">
+          PicGenie
+        </a>
+      </h2>
+      <p>
+        WenApp that takes picture input through sms(text message) and responds
+        with a wikipedia definition of what's in the picture.
+      </p>
     </div>
   </div>
 );
